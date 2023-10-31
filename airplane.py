1d332970fcca4a841d8358ddeb58b2a62a66ee82
Namgay Wangchuk <02230290.cst@rub.edu.bt>
2023-10-31 20:55:32 +0600

    Added the instructions of the game and other things

--- airplane.py
+++ airplane.py
@@ -14,6 +14,8 @@ WIDTH, HEIGHT = 1000, 800
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
+font_size = 24
+missed_gifts = 0 
 
 # Create the screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -42,12 +44,56 @@ def create_gift():
     y = aeroplane_y + 30  # Start gift objects from the bottom of the aeroplane
     return {"x": x, "y": y}
 
-# Display the loading image
-loading_time = 2  # Time to display the loading image in seconds
+# Create a new font object with the desired font size
+font = pygame.font.Font(None, font_size)
+# Function to display instructions at the center of the screen
+def display_instructions_center():
+    instructions = [
+        "Instructions:",
+        "1. Use the Left and Right arrow keys to move the boy left and right.",
+        "2. Collect the falling gifts to increase your score.",
+        "3. You have to collect 30 gifts to win the game.",
+        "4. Avoid missing gifts, as missing too many will result in a Game Over.",
+        "5. Press 'ESC' to quit the game at any time."
+    ]
 
-start_time = time.time()
-while time.time() - start_time < loading_time:
-    screen.blit(loading_image, (0, 0))
+    y_position = (HEIGHT - len(instructions) * 30) // 2
+    for line in instructions:
+        instruction_text = font.render(line, True, BLACK)
+        text_rect = instruction_text.get_rect(left=(WIDTH // 4), top=y_position)
+        screen.blit(instruction_text, text_rect)
+        y_position += font_size + 6
+
+    return y_position  # Return the y-position after displaying instructions
+
+# Get the y-position after displaying instructions
+start_button_y = display_instructions_center() + 30  # Add some spacing
+
+# Create the "START" button
+start_button = font.render("START", True, BLACK)
+start_button_rect = start_button.get_rect(center=(WIDTH // 2, start_button_y))
+
+# Function to check if the "START" button is clicked
+def is_start_button_clicked(pos):
+    return start_button_rect.collidepoint(pos)
+
+# Main game loop
+game_started = False
+
+while not game_started:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            game_started = True
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            if is_start_button_clicked(event.pos):
+                game_started = True
+
+    screen.blit(background, (0, 0))  # Set the background image
+    display_instructions_center()
+    screen.blit(start_button, start_button_rect)
     pygame.display.update()
 
 # Main game loop
@@ -78,10 +124,11 @@ while in_game:
         if boy_x < gift_obj["x"] < boy_x + 40 and boy_y < gift_obj["y"] < boy_y + 40:
             gift_objects.remove(gift_obj)
             score += 1
-
+        elif gift_obj["y"] >= HEIGHT:  # Check if the gift has gone off the screen
+            missed_gifts += 1
     gift_objects = [gift_obj for gift_obj in gift_objects if gift_obj["y"] < HEIGHT]
 
-    if total_gifts >= 50:
+    if total_gifts >= 30:
         in_game = False
 
     keys = pygame.key.get_pressed()
@@ -97,7 +144,10 @@ while in_game:
     
     screen.blit(boy, (boy_x, boy_y))
     score_text = font.render(f"Score: {score}", True, BLACK)
+    missed_gifts_text = font.render(f"Missed Gifts: {missed_gifts}", True, BLACK)  # Display missed gifts
     screen.blit(score_text, (10, 10))
+    screen.blit(missed_gifts_text, (10, 10 + font_size + 6))  # Adjust vertical position
+    
     pygame.display.update()
     clock.tick(60)
 
@@ -105,14 +155,23 @@ while in_game:
 screen.fill(WHITE)
 pygame.display.update()
 
-# Display "Game Over" when the game ends
+# Display "Game Over" and "You have scored" at the center
 game_over_text = font.render("Game Over", True, RED)
-screen.blit(game_over_text, (WIDTH // 2 - 70, HEIGHT // 2 - 18))
+game_over_rect = game_over_text.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 18))
 score_text = font.render(f"You have scored {score} points", True, BLACK)
-screen.blit(score_text, (WIDTH // 2 - 190, HEIGHT // 2 + 18))
+score_rect = score_text.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 18))
+
+# Change the background to white for the Game Over screen
+screen.fill(WHITE)
+pygame.display.update()
+
+# Display "Game Over" and "You have scored" at the center
+screen.blit(game_over_text, game_over_rect)
+screen.blit(score_text, score_rect)
 pygame.display.update()
 time.sleep(4)  # Display "Game Over" for 4 seconds
 
 # Quit Pygame
 pygame.quit()
 sys.exit()
+
